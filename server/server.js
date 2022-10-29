a7396cc9c12f0f8c18303ab5eb0f90ddfe3472d7
Shawn Smith <sds.smith24@gmail.com>
2022-10-29 10:10:01 -0400

    add mongodb uri, user_type and mutation

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://sds:rPg9SMWaeXlTk5v5@cluster0.ldkor3j.mongodb.net/?retryWrites=true&w=majority';
+// const MONGO_URI = 'mongodb://sds:rPg9SMWaeXlTk5v5@cluster0.ldkor3j.mongodb.net/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
